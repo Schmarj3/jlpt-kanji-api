0c99bb3be6a7ccd591be407ce9c2d7bcdd2b0432
Marj Etienne <marj.etienne@trupanion.com>
2021-04-27 21:16:21 -0700

    added xml serialization formatter

--- JLPTKanji/Startup.cs
+++ JLPTKanji/Startup.cs
@@ -16,7 +16,9 @@ namespace JLPTKanji
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services
+                .AddControllers(o => o.RespectBrowserAcceptHeader = true)
+                .AddXmlDataContractSerializerFormatters();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
