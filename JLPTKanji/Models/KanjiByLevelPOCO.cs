76913d4390004b963ec4f13ff8a08a87968f9872
Marj Etienne <marj.etienne@trupanion.com>
2021-04-27 19:13:59 -0700

    temporarily commented out code for ef

---
+++ JLPTKanji/Models/KanjiByLevelPOCO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JLPTKanji.Model
+{
+    public class KanjiByLevelPOCO
+    {
+        /*public KanjiByLevelModel(int id, int jlptlevel, List<string> kanji)
+        {
+            Id = id;
+            JLPTLevel = jlptlevel;
+            Kanji = kanji;
+
+        }
+
+        private KanjiByLevelModel()
+        {
+
+        }*/
+
+        public int Id { get; set; }
+        public int JLPTLevel { get; set; }
+        public List<string> Kanji { get; set; }
+
+    }
+}
