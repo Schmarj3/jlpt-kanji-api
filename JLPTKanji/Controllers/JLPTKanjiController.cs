a26069a297fb19093e1f237870ea6d0f947df72f
Marj Etienne <marj.etienne@trupanion.com>
2021-04-27 19:39:11 -0700

    added get by jlpt level endpoint

--- JLPTKanji/Controllers/JLPTKanjiController.cs
+++ JLPTKanji/Controllers/JLPTKanjiController.cs
@@ -16,41 +16,10 @@ namespace JLPTKanji.Controllers
             return new JsonResult(TempKanjiDataStore.Current.KanjiLists);
         }
 
-        /*[HttpGet("{JLPTLevel}")]
-        public JsonResult GetKanjiByLevel()
+        [HttpGet("{jlptlevel}")]
+        public JsonResult GetJlptKanjiByLevel(int jlptlevel)
         {
-            var KanjiList = new List<object>()
-                {
-                    new {
-                        id = 1,
-                        JLPTLevel = 5,
-                        Kanji = new List<string> { "ichi","ni" }
-                    },
-                    new {
-                        id = 2,
-                        JLPTLevel = 4,
-                        Kanji = new List<string> { "san","shi" }
-                    }
-                    ,
-                    new {
-                        id = 3,
-                        JLPTLevel = 3,
-                        Kanji = new List<string> { "go","roku" }
-                    },
-                    new {
-                        id = 4,
-                        JLPTLevel = 2,
-                        Kanji = new List<string> { "nana","hachi" }
-                    },
-                    new {
-                        id = 5,
-                        JLPTLevel = 1,
-                        Kanji = new List<string> { "kyuu","jyuu" }
-                    }
-                };
-
-            var KanjiByLevel = KanjiList.FirstOrDefault(l => l.JLPTLevel == JLPTLevel)
-            return
-        }*/
+            return new JsonResult(TempKanjiDataStore.Current.KanjiLists.FirstOrDefault(kanji => kanji.JLPTLevel == jlptlevel));
+        }
     }
 }
